649f15d1679ef520bf2f7f8b769af95c69029047
TheMightyPong <6806519+PongAlmighty@users.noreply.github.com>
2025-01-06 18:54:38 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from app import app, socketio
 if __name__ == "__main__":
     # Use environment variable with fallback to port 8765
     port = int(os.environ.get('PORT', 8765))
-    socketio.run(app, host="0.0.0.0", port=port, debug=True, use_reloader=True, log_output=True)
+    socketio.run(app, host="0.0.0.0", port=port, debug=True, use_reloader=True, log_output=True, allow_unsafe_werkzeug=True)
